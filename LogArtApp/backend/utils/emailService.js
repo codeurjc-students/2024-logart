1b8298d6156520d1359a6db07a3031ab754ff1ea
DavidMorenoo <davidmorenom17@gmail.com>
2024-11-28 20:33:33 +0100

    feat: Implement email verification for user registration
    -Add isVerified field to the user model to track email verification status.
    -Generate and send a unique verification token upon registration.
    -Implement logic to prevent login if the email has not been verified.
    -Create route to handle verification of the token sent via email.
    -Enhance user experience by notifying users about the need to verify their email.

--- LogArtApp/backend/utils/emailService.js
+++ LogArtApp/backend/utils/emailService.js
@@ -1,22 +1,25 @@
 const nodemailer = require('nodemailer');
+const { email } = require('../config/environment');
+const { password } = require('../config/environment');
 
 const sendVerificationEmail = async (to, link) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER, 
-      pass: process.env.EMAIL_PASS, 
+      user: email,
+      pass: password,
     },
   });
 
   const mailOptions = {
-    from: process.env.EMAIL_USER,
+    from: email,
     to,
-    subject: 'Verify your email',
+    subject: 'Verify your LogArt email',
     html: `<p>Click the link below to verify your email:</p><a href="${link}">${link}</a>`,
   };
-
-  await transporter.sendMail(mailOptions);
+  
+    const info = await transporter.sendMail(mailOptions);
+    
 };
 
 module.exports = sendVerificationEmail;
